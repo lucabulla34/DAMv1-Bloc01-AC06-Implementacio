eee78fbaa37d19df7c910e95c8f6a1764c930f23
lucabulla34 <lucabulla14@gmail.com>
2025-10-16 10:32:14 +0200

    First Commit; Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorldProject
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            const string HelloWorld = "Hello World!";
+            Console.WriteLine(HelloWorld);
+        }
+    }
+}
